5a6007ea341141282c92a7b8aefb32048dfd154b
Ca2le <carl@jumpcode.org>
2024-01-10 09:28:24 +0100

    fixed bug

--- src/store/redux_slices/auth.slice.ts
+++ src/store/redux_slices/auth.slice.ts
@@ -1,48 +1,21 @@
-import { PayloadAction, ThunkAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { IUser } from "../../types";
-import axios from "../../utils/axios";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
-export interface authState {
-    isAuth: boolean,
-    isLoading: boolean
-    currentUser: IUser | null,
-    error: null | Error,
-    success: boolean
-}
 
 const initialState = {
-    isAuth: false,
-    isLoading: false,
-    currentUser: null,
-    error: null,
-    success: true
+    isAuth: false
 }
-
-const loginUser = createAsyncThunk('auth/signIn', () => {
-    return async ({ email, password }: IUser) => {
-        const respone = await axios.post('/login', { email, password })
-       respone.data
-        
-    }
-})
-
-
 const authSlice = createSlice({
     name: 'auth',
     initialState,
     reducers: {
         isAuth(state, action: PayloadAction<boolean>) {
             state.isAuth = action.payload
-        },
-        isLoading(state, action: PayloadAction<boolean>) {
-            state.isLoading = action.payload
-        },
-
+        }
     }
 })
 
-authSlice.actions.setAuth(true)
 
-export const { setAuth } = authSlice.actions
+
+export const { isAuth } = authSlice.actions
 
 export default authSlice.reducer
